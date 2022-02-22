066a91943fc5165f3a46e8b3eeb9755291c16526
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2022-02-22 22:40:47 +1030

    init

---
+++ hello.cbl
@@ -0,0 +1,8 @@
+      *     Hello World Program
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. hello_world.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, world".
+           STOP RUN.
+        
+           
